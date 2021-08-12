814912e070b988133d065159ef5ff6a80cc63bd4
Pavel Dubov <PADking888@yandex.ru>
2021-08-12 19:53:48 +0300

    feat: Found all active passcards for visual comparison

--- Django-ORM-standalone/main.py
+++ Django-ORM-standalone/main.py
@@ -10,23 +10,19 @@ from datacenter.models import Passcard  # noqa: E402
 
 def main():
     passcards = Passcard.objects.all()
-    passcard = passcards[0]
 
-    owner_name = passcard.owner_name
-    passcode = passcard.passcode
-    created_at = passcard.created_at
-    is_active = passcard.is_active
+    active_passcards = []
+    for passcard in passcards:
+        if passcard.is_active:
+            active_passcards.append(passcard)
 
-    passcard_template = """
-        owner_name: {}
-        passcode: {}
-        created_at: {}
-        is_active: {}
-    """
-    passcard = passcard_template.format(owner_name, passcode,
-                                        created_at, is_active)
+    passcards_count_template = "Всего пропусков {}"
+    passcards_count = passcards_count_template.format(len(passcards))
 
-    print(passcard)
+    active_passcards_template = "Активных пропусков {}"
+    active_passcards = active_passcards_template.format(len(active_passcards))
+
+    print(passcards_count, active_passcards, sep="\n")
 
 
 if __name__ == '__main__':
