9c1d084396263d65077b3de0814a13f0912a818c
Pavel Dubov <PADking888@yandex.ru>
2021-08-12 19:23:23 +0300

    feat: The properties of one of the passcards are displayed

--- Django-ORM-standalone/main.py
+++ Django-ORM-standalone/main.py
@@ -7,6 +7,27 @@ django.setup()
 
 from datacenter.models import Passcard  # noqa: E402
 
-if __name__ == '__main__':
+
+def main():
     passcards = Passcard.objects.all()
-    print(passcards)
+    passcard = passcards[0]
+
+    owner_name = passcard.owner_name
+    passcode = passcard.passcode
+    created_at = passcard.created_at
+    is_active = passcard.is_active
+
+    passcard_template = """
+        owner_name: {}
+        passcode: {}
+        created_at: {}
+        is_active: {}
+    """
+    passcard = passcard_template.format(owner_name, passcode,
+                                        created_at, is_active)
+
+    print(passcard)
+
+
+if __name__ == '__main__':
+    main()
