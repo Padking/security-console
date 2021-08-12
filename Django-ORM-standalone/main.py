f41741141d2a96b874c77e331dc53876639971c1
Pavel Dubov <PADking888@yandex.ru>
2021-08-12 20:00:54 +0300

    feat: Found all active passcards with `filter` for visual comparison

--- Django-ORM-standalone/main.py
+++ Django-ORM-standalone/main.py
@@ -9,20 +9,16 @@ from datacenter.models import Passcard  # noqa: E402
 
 
 def main():
-    passcards = Passcard.objects.all()
-
-    active_passcards = []
-    for passcard in passcards:
-        if passcard.is_active:
-            active_passcards.append(passcard)
+    passcards_count = Passcard.objects.count()
+    active_passcards = Passcard.objects.filter(is_active=True)
 
     passcards_count_template = "Всего пропусков {}"
-    passcards_count = passcards_count_template.format(len(passcards))
+    passcards_count_msg = passcards_count_template.format(passcards_count)
 
     active_passcards_template = "Активных пропусков {}"
     active_passcards = active_passcards_template.format(len(active_passcards))
 
-    print(passcards_count, active_passcards, sep="\n")
+    print(passcards_count_msg, active_passcards, sep="\n")
 
 
 if __name__ == '__main__':
