6bfccd084037927c965fbfb7fe16b31ac5ace75e
Pavel Dubov <PADking888@yandex.ru>
2021-08-12 17:26:37 +0300

    feat: Get all passcards from database

--- Django-ORM-standalone/main.py
+++ Django-ORM-standalone/main.py
@@ -8,5 +8,5 @@ django.setup()
 from datacenter.models import Passcard  # noqa: E402
 
 if __name__ == '__main__':
-    # Программируем здесь
-    print('Количество пропусков:', Passcard.objects.count())  # noqa: T001
+    passcards = Passcard.objects.all()
+    print(passcards)
